cb441170f99d0de2dc24d43cbc1f5af66b7101a8
jonas <jonas.saslona@gmx.de>
2024-11-25 16:59:59 +0100

    more cleanup

--- src/main/java/de/jonas/customitems/CustomItems.java
+++ src/main/java/de/jonas/customitems/CustomItems.java
@@ -1,8 +1,5 @@
 package de.jonas.customitems;
 
-import de.jonas.customitems.commands.CreateItemCommand;
-import de.jonas.customitems.commands.GiveItemCommand;
-import de.jonas.customitems.commands.ItemCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 import dev.jorel.commandapi.CommandAPI;
 import dev.jorel.commandapi.CommandAPIBukkitConfig;
@@ -16,33 +13,18 @@ public final class CustomItems extends JavaPlugin {
     public static CustomItems INSTANCE;
     public static String PREFIX;
     public Logger logger;
-    public DataBasePool dbPool;
 
     public void onLoad() {
         INSTANCE = this;
-        PREFIX = "[CI] ";
-
-        dbPool = new DataBasePool();
 
         this.logger = this.getLogger();
 
         CommandAPI.onLoad(new CommandAPIBukkitConfig(this));
-        new GiveItemCommand();
-        new CreateItemCommand();
-        new ItemCommand();
     }
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
-        dbPool.init();
-
-        try {
-            dbPool.createTable();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
 
         CommandAPI.onEnable();
 
